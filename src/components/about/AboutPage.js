c59a569de2785b7ef87282e5b503dbda4748d71b
Prabin Sunar <sunarprabin.ps@gmail.com>
2020-11-16 22:15:13 +1100

    About and Home Components - demo

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AboutPage = () => {
+    <div>
+        <h2>About</h2>
+        <p>This app uses React, Redux, React router and other useful libraries.</p>
+    </div>
+};
+
+export default AboutPage;
